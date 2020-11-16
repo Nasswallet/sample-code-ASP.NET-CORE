9d0e82e23b18a8ee865453b5bfb14e7842a20c63
Nasswallet <omar.ali@nasswallet.com>
2020-11-17 00:39:07 +0300

    inital commit

---
+++ sample-code-ASP.NET Core/Controllers/PaymentController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using sample_code_ASP.NET_Core.Models;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+
+
+namespace sample_code_ASP.NET_Core.Controllers
+{
+        
+    public class PaymentController : Controller
+    {
+        private readonly ILogger<PaymentController> _logger;
+
+        public PaymentController(ILogger<PaymentController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> callAPI()
+        {
+     
+            const string userIdentifier = "7503491464";  //Your merchant account identifier
+            const string password = "Nass#2020";    //Your merchant account password
+            const string transactionPin = "135758";  //Your merchant account MPIN
+            const string basicToken = "TUVSQ0hBTlRfQVBQOk1lcmNoYW50QEFkbWluIzEyMw==";
+            const string headerauth = "Basic";
+            
+
+
+            SampleApiCall callApi = new SampleApiCall();
+            var request = new
+            {
+                data = new
+                {
+                    username = userIdentifier,
+                    password = password,
+                    grantType = "password"
+
+                }
+            };
+            
+
+            var req = JsonConvert.SerializeObject(request);
+           
+             var response = await callApi.PostAsync<ResponseModel>("login", request, headerauth, basicToken);
+
+            if (response.ResponseCode == 0)
+            {
+                var initrequest = new
+                {
+                    data = new
+                    {
+                        userIdentifier = userIdentifier,
+                        transactionPin = transactionPin,
+                        orderId = "263626",   //order ID which will be provided by the merchant.
+                        amount = "10",  //order amount/price will be provided by the merchant.
+                        languageCode = "en",  
+                    }
+                };
+                dynamic datavalues = JObject.Parse(response.Data.ToString());
+                string bearertoken = datavalues.access_token;
+                var initresponse = await callApi.PostAsync<ResponseModel>("initTransaction", initrequest, "Bearer", bearertoken);
+                if (initresponse.ResponseCode == 0)
+                {
+                    dynamic transactionValues = JObject.Parse(initresponse.Data.ToString());
+                    string txn_id = transactionValues.transactionId;
+                    string txn_token = transactionValues.token;
+                    string urltoRedirect = "https://uatcheckout.nasswallet.com/payment-gateway?id=" + txn_id + "&token=" + txn_token + "&userIdentifier=" + userIdentifier;
+                     return Json(urltoRedirect);
+
+                }
+                return Json(initresponse.Data);
+            }
+            return Json(response.Data);
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ sample-code-ASP.NET Core/Models/SampleApiCall.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using System.Net.Http.Formatting;
+
+namespace sample_code_ASP.NET_Core.Models
+{
+    public class SampleApiCall
+    {
+        private string _baseUrl = "https://uatgw.nasswallet.com";
+
+        #region Ctor
+        public SampleApiCall()
+        {
+
+
+        }
+        #endregion
+
+        #region Methods
+
+
+        public async Task<T> PostAsync<T>(string apiMethod, object model, string headerauth, string headerauthvalue)
+        {
+            try
+            {
+                //Create Request
+                Uri _baseUrlUri = new Uri(_baseUrl);    
+                HttpClient _client = new HttpClient();
+                _client.BaseAddress = _baseUrlUri;
+                _client.DefaultRequestHeaders.Accept.Clear();
+                _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                _client.DefaultRequestHeaders.Add("Authorization", headerauth + " " + headerauthvalue);
+                _client.Timeout = TimeSpan.FromMinutes(10);
+
+                //Format Url
+                string url = string.Empty;
+                url = _baseUrl + "/payment/transaction/" + apiMethod;
+                            
+                //Call Api
+                HttpResponseMessage responseMessage = null;
+                responseMessage = await _client.PostAsJsonAsync(url, model);
+
+                if (responseMessage.StatusCode == HttpStatusCode.OK)
+                {
+                    var responseData = responseMessage.Content.ReadAsStringAsync().Result;
+                    var apiData = JsonConvert.DeserializeObject<T>(responseData);
+                    return apiData;
+                }
+                else
+                {
+                    var obj = (T)Activator.CreateInstance(typeof(T));
+                    return obj;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        #endregion
+
+    }
+}
+
+public class ResponseModel
+{
+    public int ResponseCode { get; set; }
+    public int ErrCode { get; set; }
+    public string Message { get; set; }
+    public object Data { get; set; }
+}
